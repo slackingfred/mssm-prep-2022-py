a6e087244982c15cb621ab6a22b758b18b796b48
Fred <fredqian@cmu.edu>
2022-07-06 07:46:57 +0800

    Save...

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
+import csv
+import recommend
+import sys
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) != 2:
+        print('Please specify new user list file')
+        sys.exit(1)
+    with open('outputs/intermediate.csv', 'r') as fin:
+        engine = recommend.Recommender(fin)
+    with open(sys.argv[1], 'r') as newusers:
+        rdr = csv.DictReader(newusers)
+        #
+
